ef7498648a30636bfe51227f39ae7fb4b0135112
Heshara13 <nirmithaheshara527@gmail.com>
2025-03-12 11:55:52 +0530

    display data with filters

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:zenflow/pages/main_screen.dart';
+import 'package:zenflow/providers/filter_provider.dart';
+import 'package:zenflow/providers/meditation_provider.dart';
+import 'package:zenflow/providers/mindfull_exercise_provider.dart';
+import 'package:zenflow/providers/sleep_content_provider.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("zenflow")),
-        body: Text("default"),
-      ),
-    );
-  }
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => MindfullExerciseProvider()),
+        ChangeNotifierProvider(create: (context) => MeditationProvider()),
+        ChangeNotifierProvider(create: (context) => SleepContentProvider()),
+        ChangeNotifierProvider(create: (context) => FilterdDataprovider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
---
+++ lib/providers/filter_provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:zenflow/models/meditation_model.dart';
+import 'package:zenflow/models/mindfull_exercise_model.dart';
+import 'package:zenflow/models/sleep_content_model.dart';
+import 'package:zenflow/providers/meditation_provider.dart';
+import 'package:zenflow/providers/mindfull_exercise_provider.dart';
+import 'package:zenflow/providers/sleep_content_provider.dart';
+
+class FilterdDataprovider extends ChangeNotifier {
+  List<dynamic> _allData = [];
+  List<dynamic> _filteredData = [];
+
+  List<dynamic> get filteredData => _filteredData;
+
+  Future<void> getData(BuildContext context) async {
+    await Future.delayed(Duration.zero); // Ensures this runs after build
+
+    final List<MindfulnessExercise> mindfullExercise =
+        Provider.of<MindfullExerciseProvider>(
+          context,
+          listen: false,
+        ).mindfullExercise;
+
+    final List<SleepContent> sleepContent =
+        Provider.of<SleepContentProvider>(context, listen: false).sleepContents;
+
+    final List<MeditationContent> meditations =
+        Provider.of<MeditationProvider>(context, listen: false).meditations;
+
+    _allData = [...mindfullExercise, ...sleepContent, ...meditations];
+
+    _filteredData = _allData;
+    notifyListeners();
+  }
+
+  void filterData(String category) {
+    if (category == "All") {
+      _filteredData = _allData;
+    } else if (category == "Mindfulness") {
+      _filteredData = _allData.whereType<MindfulnessExercise>().toList();
+    } else if (category == "Meditation") {
+      _filteredData = _allData.whereType<MeditationContent>().toList();
+    } else if (category == "Sleep Stories") {
+      _filteredData = _allData.whereType<SleepContent>().toList();
+    }
+    notifyListeners();
+  }
+}
