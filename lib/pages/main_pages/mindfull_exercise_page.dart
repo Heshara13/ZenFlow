7c73d38a8cd52ce2c021fbca7b3aa0ee72a356d9
Heshara13 <nirmithaheshara527@gmail.com>
2025-03-16 19:03:00 +0530

    create mindfull_exercise_page & add changes to main_screen page

---
+++ lib/pages/main_pages/mindfull_exercise_page.dart
@@ -0,0 +1,126 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:zenflow/models/mindfull_exercise_model.dart';
+import 'package:zenflow/providers/mindfull_exercise_provider.dart';
+import 'package:zenflow/router/router_names.dart';
+import 'package:zenflow/utils/colors.dart';
+import 'package:zenflow/utils/text_styles.dart';
+
+class MindFullExercisePage extends StatelessWidget {
+  const MindFullExercisePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Mindfull Exercises',
+          style: TextStyle(
+            fontSize: 29,
+            fontWeight: FontWeight.bold,
+            color: AppColors.primaryPurple,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                // Search bar
+                TextField(
+                  onChanged: (value) {
+                    Provider.of<MindfullExerciseProvider>(
+                      context,
+                      listen: false,
+                    ).searchMindfullExercise(value);
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    prefixIcon: const Icon(Icons.search),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: AppColors.primaryPurple,
+                      ),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: AppColors.primaryPurple,
+                      ),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                // List of mindfulness activities
+                Consumer<MindfullExerciseProvider>(
+                  builder: (context, mindfulnessExerciseProvider, child) {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount:
+                          mindfulnessExerciseProvider.mindfullExercise.length,
+                      itemBuilder: (context, index) {
+                        MindfulnessExercise mindfullExercise =
+                            mindfulnessExerciseProvider.mindfullExercise[index];
+                        return Container(
+                          margin: const EdgeInsets.symmetric(vertical: 5),
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryDarkBlue.withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ListTile(
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.asset(
+                                mindfullExercise.imagePath,
+                                width: MediaQuery.of(context).size.width * 0.13,
+                                height:
+                                    MediaQuery.of(context).size.width * 0.13,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: Text(
+                              mindfullExercise.name,
+                              style: AppTextStyles.subtitleStyle,
+                            ),
+                            subtitle: Text(
+                              mindfullExercise.description,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: AppTextStyles.bodyStyle.copyWith(
+                                color: AppColors.primaryDarkBlue.withOpacity(
+                                  0.7,
+                                ),
+                                fontSize: 14,
+                              ),
+                            ),
+                            onTap: () {
+                              GoRouter.of(context).pushNamed(
+                                RouteNames.mindFullExercise,
+                                queryParameters: {
+                                  'mindfullExercise': jsonEncode(
+                                    mindfullExercise.toJson(),
+                                  ),
+                                },
+                              );
+                            },
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:zenflow/pages/main_pages/create_custom_exercise.dart';
 import 'package:zenflow/pages/main_pages/custum_exercises.dart';
 import 'package:zenflow/pages/main_pages/home_page.dart';
-import 'package:zenflow/pages/main_pages/meditation_page.dart';
+import 'package:zenflow/pages/main_pages/mindfull_exercise_page.dart';
 import 'package:zenflow/pages/main_pages/profile_page.dart';
-import 'package:zenflow/pages/main_pages/stats_page.dart';
+import 'package:zenflow/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,10 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MainScreen(),
-    );
+    return const MaterialApp(title: 'Flutter Demo', home: MainScreen());
   }
 }
 
@@ -34,10 +32,10 @@ class _MainScreenState extends State<MainScreen> {
 
   static const List<Widget> _pages = <Widget>[
     HomePage(),
-    MindFullE
+    MindFullExercisePage(),
     CreateCustomExercise(),
     CustomExercises(),
-    ProfilePage()
+    ProfilePage(),
   ];
 
   void _onItemTapped(int index) {
@@ -49,19 +47,86 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Use a transparent background
       body: Center(child: _pages.elementAt(_selectedIndex)),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Mediation'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Create'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Stats'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.circular(100),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/home.svg',
+                colorFilter: ColorFilter.mode(
+                  _selectedIndex == 0
+                      ? AppColors.primaryPurple
+                      : AppColors.primaryGrey,
+                  BlendMode.srcIn,
+                ),
+                semanticsLabel: 'My SVG Image',
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/brain.svg',
+                colorFilter: ColorFilter.mode(
+                  _selectedIndex == 1
+                      ? AppColors.primaryPurple
+                      : AppColors.primaryGrey,
+                  BlendMode.srcIn,
+                ),
+                semanticsLabel: 'My SVG Image',
+                height: 25,
+                width: 25,
+              ),
+              label: 'Mediation',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/circle-plus.svg',
+                colorFilter: ColorFilter.mode(
+                  _selectedIndex == 2
+                      ? AppColors.primaryPurple
+                      : AppColors.primaryGrey,
+                  BlendMode.srcIn,
+                ),
+                semanticsLabel: 'My SVG Image',
+              ),
+              label: 'Create',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/file-plus-2.svg',
+                colorFilter: ColorFilter.mode(
+                  _selectedIndex == 3
+                      ? AppColors.primaryPurple
+                      : AppColors.primaryGrey,
+                  BlendMode.srcIn,
+                ),
+                semanticsLabel: 'My SVG Image',
+              ),
+              label: 'Stats',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/user-round-cog.svg',
+                colorFilter: ColorFilter.mode(
+                  _selectedIndex == 4
+                      ? AppColors.primaryPurple
+                      : AppColors.primaryGrey,
+                  BlendMode.srcIn,
+                ),
+                semanticsLabel: 'My SVG Image',
+              ),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: AppColors.primaryPurple,
+          unselectedItemColor: AppColors.primaryDarkBlue,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
