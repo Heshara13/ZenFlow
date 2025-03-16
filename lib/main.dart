94848f5e94e9b0f32fb5ed205ed2ee9672118ae0
Heshara13 <nirmithaheshara527@gmail.com>
2025-03-16 19:41:35 +0530

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'zenflow',
       theme: ThemeData(
         primarySwatch: Colors.blue,
